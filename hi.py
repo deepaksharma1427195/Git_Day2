d0f05e02dbc6bb02f83b7773206b55a553735d80
deepaksharma1427195 <57553789+deepaksharma1427195@users.noreply.github.com>
2025-03-20 10:20:46 +0530

    Prime Number in Python

--- hi.py
+++ hi.py
@@ -1,10 +1,10 @@
 n = int(input("Enter Number: "))
 if (n<2):
-    print(f"{n} is neither prime nor composite")
+	print(f"{n} is neither prime nor composite")
 elif (n>=2):
-    for i in range(2,n):
-        if(n%i == 0):
-	    print(f"{n} is not prime")
-	    break
-     else:
-         print(f"{n} is Prime Number")
+	for i in range(2,n):
+		if(n%i == 0):
+			print(f"{n} is not prime")
+			break
+	else:
+		print(f"{n} is Prime Number")
